adda2a9ce3bbdec500a7979df7adedc9a5917373
unknown <y-shimada@.(none)>
2009-09-17 13:45:18 +0900

    initial commit

---
+++ lib/CGI/Param/Filter.pm
@@ -0,0 +1,41 @@
+package CGI::Param::Filter;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+CGI::Param::Filter -
+
+=head1 SYNOPSIS
+
+  use CGI::Param::Filter;
+
+=head1 DESCRIPTION
+
+CGI::Param::Filter is
+
+=head1 METHODS
+
+=over
+
+=item xxx
+
+=back
+
+=head1 AUTHOR
+
+Yuji Shimada E<lt>xaicron {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
